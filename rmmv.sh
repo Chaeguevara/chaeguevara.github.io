55fa8e6e2814b740891079fcce813c18ca6daae5
Chaeguevara <dfso2@naver.com>
2023-10-01 02:25:55 +0900

    update shell for exception files

--- rmmv.sh
+++ rmmv.sh
@@ -2,4 +2,4 @@
 
 echo "changed";
 rm -rf ~/Documents/2023/quartz/content/*;
-cp -R ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/MyFirstVault/*  ~/Documents/2023/quartz/content/;
+cp -R ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/MyFirstVault/[!_]*  ~/Documents/2023/quartz/content/;
